5b215291448301e440a759452245c60d4293b46c
benoit <benoit.triay+13@gmail.com>
2018-03-14 22:22:35 +0000

    Simplify DB

--- src/create_db.sql
+++ src/create_db.sql
@@ -7,23 +7,11 @@ create table GAME(
 
 create table PLAYER(
 	player_id integer primary key,
-	name text not null);
-
-create table PLAYER_COLLECTION(
-	collection_id integer primary key,
-	player_id integer not null,
-	foreign key (player_id) references PLAYER(player_id) on delete cascade);
+	name text not null;
 
 create table COLLECTION(
-	collection_id integer not null,
-	game_id integer not null,
-	foreign key (collection_id) references PLAYER_COLLECTION(collection_id) on delete cascade,
-	foreign key (game_id) references GAME(game_id) on delete cascade);
-
-create table PLAYER_RATING(
 	player_id integer not null,
 	game_id integer not null,
-	rating integer not null,
+	rating integer,
 	foreign key (player_id) references PLAYER(player_id) on delete cascade,
 	foreign key (game_id) references GAME(game_id) on delete cascade);
-
